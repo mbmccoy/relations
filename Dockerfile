bb3db9a25c355bc265779d375a7c9006550e6ce2
Michael McCoy <michael.b.mccoy@gmail.com>
2024-10-30 11:01:02 -0700

    Now have a working jupyter server

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM nvcr.io/nvidia/pytorch:24.05-py3
 #FROM pytorch/pytorch:2.1.2-cuda12.1-cudnn8-runtime
 
+# Install Node.js and npm
+RUN apt-get update && \
+    apt-get install -y curl && \
+    curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && \
+    apt-get install -y nodejs && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 RUN mkdir -p /workspace
 WORKDIR /workspace
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Check if UID and GID are provided
-if [[ -z "$UID" || -z "$GID" ]]; then
-  echo -e "\e[31m[ERROR] UID and GID environment variables are not set.\e[0m"
-  echo -e "\e[33mTo run this container properly, you need to pass your user and group IDs.\e[0m"
+# Check if UID, GID, and USER are provided
+if [[ -z "$UID" || -z "$GID" || -z "$USER" || -z "$JUPYTER_PORT" ]]; then
+  echo -e "\e[31m[ERROR] UID, GID, and USER environment variables are not set.\e[0m"
+  echo -e "\e[33mTo run this container properly, you need to pass your user, group IDs, and username.\e[0m"
   echo -e "\e[33mYou can do this in one of the following ways:\e[0m"
-  echo -e "\e[32m1. Pass UID and GID when calling docker-compose, e.g.,\e[0m"
-  echo -e "\e[32m   UID=$(id -u) GID=$(id -g) docker-compose up\e[0m"
-  echo -e "\e[32m2. Create an .env file in the project directory with the following contents:\e[0m"
-  echo -e "\e[32m   UID=$(id -u)\e[0m"
-  echo -e "\e[32m   GID=$(id -g)\e[0m"
+  echo -e "\e[32m1. Pass UID, GID, and USER when calling docker-compose, e.g.,\e[0m"
+  echo -e "\e[32m   UID=\$(id -u) GID=\$(id -g) USER=\$(id -un) docker-compose up\e[0m"
+  echo -e "\e[32m2. Run the following one-liner to add them to the .env file:\e[0m"
+  echo -e "\e[32m   echo -e \"UID=\$(id -u)\\nGID=\$(id -g)\\nUSER=\$(id -un)\\\\JUPYTER_PORT=$(( $(id -u) + 10000 ))\" >> .env\e[0m"
   echo -e "\e[31mExiting now.\e[0m"
   exit 1
 fi
 
 # Set permissions for the workspace directory
-USER_ID=${UID:-1000}
-GROUP_ID=${GID:-1000}
+USER_ID=${UID:-0}
+GROUP_ID=${GID:-0}
+USERNAME=${USER:-root}
 chown -R $USER_ID:$GROUP_ID /workspace
 
 # Set Jupyter runtime, config, and data directories to locations writable by the user
@@ -26,5 +26,17 @@ export JUPYTER_DATA_DIR=/workspace/.jupyter/data
 mkdir -p $JUPYTER_RUNTIME_DIR $JUPYTER_CONFIG_DIR $JUPYTER_DATA_DIR
 chown -R $USER_ID:$GROUP_ID /workspace/.jupyter
 
+# Create group if not exists
+if ! getent group $GROUP_ID &>/dev/null; then
+  groupadd -g $GROUP_ID $USERNAME
+fi
+
+# Create user if not exists
+if ! id -u $USERNAME &>/dev/null; then
+  useradd -u $USER_ID -g $GROUP_ID -o -m $USERNAME
+fi
+
 # Run the command as the specified user, preserving the environment
-exec env JUPYTER_RUNTIME_DIR=$JUPYTER_RUNTIME_DIR JUPYTER_CONFIG_DIR=$JUPYTER_CONFIG_DIR JUPYTER_DATA_DIR=$JUPYTER_DATA_DIR "$@"
+exec su - $USERNAME -c "env JUPYTER_RUNTIME_DIR=$JUPYTER_RUNTIME_DIR \
+    JUPYTER_CONFIG_DIR=$JUPYTER_CONFIG_DIR \
+    JUPYTER_DATA_DIR=$JUPYTER_DATA_DIR $*"
